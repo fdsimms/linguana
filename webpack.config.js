fdc873ee38d60c9c0c1306e516f485cab9ed6beb
fdsimms <fdsimms@gmail.com>
2016-01-27 10:05:19 -0500

    Create frontend folder, begin running webpack, fix widths of buttons.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/linguana.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ]
+  }
+};
