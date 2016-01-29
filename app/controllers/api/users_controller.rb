b70628f63c30e71c7b166b79b8ce2212d0cc1c6a
fdsimms <fdsimms@gmail.com>
2016-01-28 19:11:32 -0500

    Fix typo in Api::UsersController.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,7 +4,7 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render: show
+      render :show
     else
       render json: @user.errors.full_messages, status: 422
     end
@@ -14,7 +14,7 @@ class Api::UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update
-      render: show
+      render :show
     else
       render json: @user.errors.full_messages, status: 422
     end
