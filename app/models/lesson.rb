faf9492492bc4a706e72846b3a0162b01cdf2ad0
fdsimms <fdsimms@gmail.com>
2016-01-28 19:16:38 -0500

    Fix validation bug in Lesson model.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,6 @@
 class Lesson < ActiveRecord::Base
   validates :name, :skill_id, presence: true
-  validates :name, unique: true
+  validates :name, uniqueness: true
 
   belongs_to :skill, dependent: :destroy
   belongs_to :course, through: :skill
