63765f96fa6aa73922e257963918b99d16eeff85
Frankie <fdsimms@gmail.com>
2016-02-17 10:13:34 -0500

    Fix default url for missing profile pics.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates :fname, :email, :points, presence: true
   validates :streak_length, :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
-  has_attached_file :profile_pic, styles: {nav: "40x40#", prof: "120x120#"}, default_url: "/assets/missing.png"
+  has_attached_file :profile_pic, styles: {nav: "40x40#", prof: "120x120#"}, default_url: "/app/assets/missing.png"
   validates_attachment_content_type :profile_pic, content_type: /\Aimage\/.*\Z/
 
   after_initialize :ensure_session_token
