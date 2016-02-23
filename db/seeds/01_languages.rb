1d16ac7f66bcc4cf3d133ebdb18973a2aa776d13
Frankie <fdsimms@gmail.com>
2016-02-23 18:34:19 -0500

    Remove incorrect tips and notes from seed files.

--- db/seeds/01_languages.rb
+++ db/seeds/01_languages.rb
@@ -1,4 +1,4 @@
-Language.destroy_all
+
 def seed_image(file_name)
   File.open(File.join(Rails.root, "/app/assets/images/#{file_name}"))
 end
@@ -9,8 +9,8 @@ france_flag = seed_image("franceflag.png")
 spain_flag = seed_image("spainflag.png")
 esperanto_flag = seed_image("esperantoflag.png")
 
-english = Language.create!(name: "English", abbreviation: "en", flag: american_flag)
-french = Language.create!(name: "French", abbreviation: "fr", flag: france_flag)
-spanish = Language.create!(name: "Spanish", abbreviation: "es", flag: spain_flag)
-portuguese = Language.create!(name: "Portuguese", abbreviation: "pt", flag: brazil_flag)
-esperanto = Language.create!(name: "Esperanto", abbreviation: "eo", flag: esperanto_flag)
+english = Language.create(name: "English", abbreviation: "en", flag: american_flag)
+french = Language.create(name: "French", abbreviation: "fr", flag: france_flag)
+spanish = Language.create(name: "Spanish", abbreviation: "es", flag: spain_flag)
+portuguese = Language.create(name: "Portuguese", abbreviation: "pt", flag: brazil_flag)
+esperanto = Language.create(name: "Esperanto", abbreviation: "eo", flag: esperanto_flag)
--- db/seeds/02_courses.rb
+++ db/seeds/02_courses.rb
@@ -4,10 +4,10 @@ english = Language.find_by_name("English")
 spanish = Language.find_by_name("Spanish")
 esperanto = Language.find_by_name("Esperanto")
 
-Course.destroy_all
+# Course.destroy_all
 Skill.destroy_all
 
-spanish_for_english = Course.create!(
+spanish_for_english = Course.create(
 name: "Spanish",
 target_language_id: spanish.id,
 known_language_id: english.id
@@ -15,7 +15,7 @@ known_language_id: english.id
 
 Dir[File.join(Rails.root, 'db', 'seeds', 'spanish_skills', '*.rb')].sort.each { |seed| load seed }
 
-french_for_english = Course.create!(
+french_for_english = Course.create(
   name: "French",
   target_language_id: french.id,
   known_language_id: english.id
@@ -23,7 +23,7 @@ french_for_english = Course.create!(
 Dir[File.join(Rails.root, 'db', 'seeds', 'french_skills', '*.rb')].sort.each { |seed| load seed }
 
 
-portuguese_for_english = Course.create!(
+portuguese_for_english = Course.create(
   name: "Portuguese",
   target_language_id: portuguese.id,
   known_language_id: english.id
@@ -32,7 +32,7 @@ portuguese_for_english = Course.create!(
 Dir[File.join(Rails.root, 'db', 'seeds', 'portuguese_skills', '*.rb')].sort.each { |seed| load seed }
 
 
-esperanto_for_english = Course.create!(
+esperanto_for_english = Course.create(
   name: "Esperanto",
   target_language_id: esperanto.id,
   known_language_id: english.id
@@ -41,7 +41,7 @@ esperanto_for_english = Course.create!(
 Dir[File.join(Rails.root, 'db', 'seeds', 'esperanto_skills', '*.rb')].sort.each { |seed| load seed }
 
 
-english_for_spanish = Course.create!(
+english_for_spanish = Course.create(
   name: "Inglés",
   target_language_id: english.id,
   known_language_id: spanish.id
@@ -49,14 +49,14 @@ english_for_spanish = Course.create!(
 
 Dir[File.join(Rails.root, 'db', 'seeds', 'ingles_es_skills', '*.rb')].sort.each { |seed| load seed }
 
-english_for_french = Course.create!(
+english_for_french = Course.create(
   name: "Anglais",
   target_language_id: english.id,
   known_language_id: french.id
 )
 Dir[File.join(Rails.root, 'db', 'seeds', 'anglais_skills', '*.rb')].sort.each { |seed| load seed }
 
-english_for_portuguese = Course.create!(
+english_for_portuguese = Course.create(
   name: "Inglês",
   target_language_id: english.id,
   known_language_id: portuguese.id
@@ -66,6 +66,6 @@ Dir[File.join(Rails.root, 'db', 'seeds', 'ingles_pt_skills', '*.rb')].sort.each 
 
 
 
-User.create!(username: "guest", password: "password", bio: "He was the best of guests.",
+User.create(username: "guest", password: "password", bio: "He was the best of guests.",
              hometown: "Guestvale", fname: "G.K.", lname: "Guesterton", email: "gk@mailinator.com",
              current_course_id: spanish_for_english.id)
