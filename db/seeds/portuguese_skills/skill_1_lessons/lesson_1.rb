6388a0425f8f017e61c6eb33d19283f87463f4d5
Frankie <fdsimms@gmail.com>
2016-02-25 10:16:48 -0500

    Make first lessons for portuguese and esperanto.

--- db/seeds/portuguese_skills/skill_1_lessons/lesson_1.rb
+++ db/seeds/portuguese_skills/skill_1_lessons/lesson_1.rb
@@ -6,167 +6,167 @@ lesson = Lesson.find_or_create_by(
   skill_id: skill.id
 )
 
-  mcq1 = Exercise.find_or_create_by(
+  mcq1 = Exercise.create(
     lesson_id: lesson.id,
     thing_to_translate: "The boy",
     exercise_type: "multiple_choice"
   )
 
-  choice_1_1 = AnswerChoice.find_or_create_by(
-    body: "El niño",
+  choice_1_1 = AnswerChoice.create(
+    body: "O menino",
     exercise_id: mcq1.id,
     is_correct: true
 
   )
-  choice_1_3 = AnswerChoice.find_or_create_by(
-    body: "El hombre",
+  choice_1_3 = AnswerChoice.create(
+    body: "O homem",
     exercise_id: mcq1.id
   )
-  choice_1_4 = AnswerChoice.find_or_create_by(
-    body: "La mujer",
+  choice_1_4 = AnswerChoice.create(
+    body: "A mulher",
     exercise_id: mcq1.id
   )
 
 
-  mcq2 = Exercise.find_or_create_by(
+  mcq2 = Exercise.create(
     lesson_id: lesson.id,
     exercise_type: "multiple_choice",
     thing_to_translate: "The girl"
   )
-  choice_2_1 = AnswerChoice.find_or_create_by(
-    body: "El oso",
+  choice_2_1 = AnswerChoice.create(
+    body: "O cachorro",
     exercise_id: mcq2.id,
   )
-  choice_2_2 = AnswerChoice.find_or_create_by(
-    body: "La niña",
+  choice_2_2 = AnswerChoice.create(
+    body: "A menina",
     is_correct: true,
     exercise_id: mcq2.id
   )
-  choice_2_3 = AnswerChoice.find_or_create_by(
-    body: "La cuchara",
+  choice_2_3 = AnswerChoice.create(
+    body: "O cunhado",
     exercise_id: mcq2.id
   )
 
 
-  mcq3 = Exercise.find_or_create_by(
+  mcq3 = Exercise.create(
     lesson_id: lesson.id,
     exercise_type: "multiple_choice",
     thing_to_translate: "The dog"
   )
-  choice_3_1 = AnswerChoice.find_or_create_by(
-    body: "El niño",
+  choice_3_1 = AnswerChoice.create(
+    body: "O gato",
     exercise_id: mcq3.id,
   )
-  choice_3_2 = AnswerChoice.find_or_create_by(
-    body: "El gato",
+  choice_3_2 = AnswerChoice.create(
+    body: "O menino",
     exercise_id: mcq3.id
   )
 
-  choice_3_4 = AnswerChoice.find_or_create_by(
-    body: "El perro",
+  choice_3_4 = AnswerChoice.create(
+    body: "O cachorro",
     is_correct: true,
     exercise_id: mcq3.id
   )
 
 
-  mcq4 = Exercise.find_or_create_by(
+  mcq4 = Exercise.create(
     lesson_id: lesson.id,
     exercise_type: "multiple_choice",
     thing_to_translate: "The mother"
   )
-  choice_4_2 = AnswerChoice.find_or_create_by(
-    body: "La madre",
+  choice_4_2 = AnswerChoice.create(
+    body: "A mãe",
     is_correct: true,
     exercise_id: mcq4.id
   )
-  choice_4_3 = AnswerChoice.find_or_create_by(
-    body: "El chico",
+  choice_4_3 = AnswerChoice.create(
+    body: "O padre",
     exercise_id: mcq4.id
   )
-  choice_4_4 = AnswerChoice.find_or_create_by(
-    body: "El gato",
+  choice_4_4 = AnswerChoice.create(
+    body: "O pão",
     exercise_id: mcq4.id
   )
 
 
-  mcq5 = Exercise.find_or_create_by(
+  mcq5 = Exercise.create(
     lesson_id: lesson.id,
     exercise_type: "multiple_choice",
     thing_to_translate: "The man"
   )
 
-  choice_5_1 = AnswerChoice.find_or_create_by(
-    body: "El niño",
+  choice_5_1 = AnswerChoice.create(
+    body: "A mulher",
     exercise_id: mcq5.id,
   )
 
-  choice_5_3 = AnswerChoice.find_or_create_by(
-    body: "El hombre",
+  choice_5_3 = AnswerChoice.create(
+    body: "O homem",
     is_correct: true,
     exercise_id: mcq5.id
   )
-  choice_5_4 = AnswerChoice.find_or_create_by(
-    body: "La niña",
+  choice_5_4 = AnswerChoice.create(
+    body: "O menino",
     exercise_id: mcq5.id
   )
 
-  mcq6 = Exercise.find_or_create_by(
+  mcq6 = Exercise.create(
     lesson_id: lesson.id,
     exercise_type: "multiple_choice",
     thing_to_translate: "The baby"
   )
 
-  choice_6_1 = AnswerChoice.find_or_create_by(
-    body: "El bebé",
+  choice_6_1 = AnswerChoice.create(
+    body: "O bebê",
     is_correct: true,
     exercise_id: mcq6.id,
   )
-  choice_6_3 = AnswerChoice.find_or_create_by(
-    body: "El niño",
+  choice_6_3 = AnswerChoice.create(
+    body: "O menino",
     exercise_id: mcq6.id
   )
-  choice_6_4 = AnswerChoice.find_or_create_by(
-    body: "El oso",
+  choice_6_4 = AnswerChoice.create(
+    body: "A pessoa",
     exercise_id: mcq6.id
   )
 
-  mcq7 = Exercise.find_or_create_by(
+  mcq7 = Exercise.create(
     lesson_id: lesson.id,
     exercise_type: "multiple_choice",
     thing_to_translate: "The bear"
   )
 
-  choice_7_1 = AnswerChoice.find_or_create_by(
-    body: "El niño",
+  choice_7_1 = AnswerChoice.create(
+    body: "O menino",
     exercise_id: mcq7.id,
   )
-  choice_7_2 = AnswerChoice.find_or_create_by(
-    body: "El oso",
+  choice_7_2 = AnswerChoice.create(
+    body: "O urso",
     is_correct: true,
     exercise_id: mcq7.id
   )
-  choice_7_3 = AnswerChoice.find_or_create_by(
-    body: "La mano",
+  choice_7_3 = AnswerChoice.create(
+    body: "A mão",
     exercise_id: mcq7.id
   )
 
 
-  mcq8 = Exercise.find_or_create_by(
+  mcq8 = Exercise.create(
     lesson_id: lesson.id,
     exercise_type: "multiple_choice",
     thing_to_translate: "The cat"
   )
 
-  choice_8_1 = AnswerChoice.find_or_create_by(
-    body: "El gato",
+  choice_8_1 = AnswerChoice.create(
+    body: "O gato",
     is_correct: true,
     exercise_id: mcq8.id,
   )
-  choice_8_3 = AnswerChoice.find_or_create_by(
-    body: "La mujer",
+  choice_8_3 = AnswerChoice.create(
+    body: "A mulher",
     exercise_id: mcq8.id
   )
-  choice_8_4 = AnswerChoice.find_or_create_by(
-    body: "El jefe",
+  choice_8_4 = AnswerChoice.create(
+    body: "O chefe",
     exercise_id: mcq8.id
   )
